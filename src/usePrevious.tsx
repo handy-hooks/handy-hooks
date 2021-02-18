5cce824658cbddcdf7dc58cefb0e49c41310fbc8
Morteza <m.jenabzadeh@gmail.com>
2021-02-18 17:42:39 +0330

    add comments

--- src/usePrevious.tsx
+++ src/usePrevious.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { useInitialMount } from "./useInitialMount";
+
+/**
+ * @description returns the previous value of a prop or state
+ * @param value
+ */
 export const usePrevious = <T,>(value: T): T | undefined => {
+  // use ref to keep the track of both prev and cur value
+
   const prevRef = React.useRef<T>();
   const curRef = React.useRef(value);
   const isInitialMount = useInitialMount();
